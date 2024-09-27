255e1bd00eff120992084160e5cbaf6336e5e6a5
Алексей <chenskijaleksey@gmail.com>
2024-09-27 16:27:37 +0300

    release3

--- main.py
+++ main.py
@@ -7,9 +7,36 @@ from catboost import CatBoostRegressor
 import warnings
 import joblib
 import sklearn
+import requests
+import tempfile
+
 sklearn.set_config(transform_output="pandas")
 
 warnings.filterwarnings('ignore')
+
+
+# model_url = 'https://github.com/AlexChen-13/houses_prices/blob/main/model.cbm'
+# preprocessor_url = 'https://github.com/AlexChen-13/houses_prices/blob/main/preprocessor.pkl'
+
+
+# # Скачивание модели
+# model_response = requests.get(model_url)
+# with tempfile.NamedTemporaryFile(delete=False) as temp_model_file:
+#     temp_model_file.write(model_response.content)
+#     temp_model_file_path = temp_model_file.name
+
+# # Загрузка модели
+# model = CatBoostRegressor()
+# model.load_model(temp_model_file_path)
+
+# # Скачивание процессора
+# preprocessor_response = requests.get(preprocessor_url)
+# with tempfile.NamedTemporaryFile(delete=False) as temp_preprocessor_file:
+#     temp_preprocessor_file.write(preprocessor_response.content)
+#     temp_preprocessor_file_path = temp_preprocessor_file.name
+
+# # Загрузка процессора
+# preprocessor = joblib.load(temp_preprocessor_file_path)
 
 model = CatBoostRegressor()
 model.load_model('/home/aleksey/DS_bootcamp/houses_prices/model.cbm')
