91437c4515211663fbf6eda776c165af17373cb9
Алексей <chenskijaleksey@gmail.com>
2024-09-27 17:01:25 +0300

    release5

--- main.py
+++ main.py
@@ -9,37 +9,18 @@ import joblib
 import sklearn
 import requests
 import tempfile
+import os
 
 sklearn.set_config(transform_output="pandas")
 
 warnings.filterwarnings('ignore')
 
 
-# model_url = 'https://github.com/AlexChen-13/houses_prices/blob/main/model.cbm'
-# preprocessor_url = 'https://github.com/AlexChen-13/houses_prices/blob/main/preprocessor.pkl'
-
-
-# # Скачивание модели
-# model_response = requests.get(model_url)
-# with tempfile.NamedTemporaryFile(delete=False) as temp_model_file:
-#     temp_model_file.write(model_response.content)
-#     temp_model_file_path = temp_model_file.name
-
-# # Загрузка модели
-# model = CatBoostRegressor()
-# model.load_model(temp_model_file_path)
-
-# # Скачивание процессора
-# preprocessor_response = requests.get(preprocessor_url)
-# with tempfile.NamedTemporaryFile(delete=False) as temp_preprocessor_file:
-#     temp_preprocessor_file.write(preprocessor_response.content)
-#     temp_preprocessor_file_path = temp_preprocessor_file.name
-
-# # Загрузка процессора
-# preprocessor = joblib.load(temp_preprocessor_file_path)
+current_directory = os.path.dirname(__file__)
+model_path = os.path.join(current_directory, 'model.cbm')
 
 model = CatBoostRegressor()
-model.load_model('/home/aleksey/DS_bootcamp/houses_prices/model.cbm')
+model.load_model(model_path)
 preprocessor = joblib.load('/home/aleksey/DS_bootcamp/houses_prices/preprocessor.pkl')
 
 st.title('Предсказание цены квартиры')
